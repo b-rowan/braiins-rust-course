fccab9bb792305675384f830353874a4dde83466
b-rowan <brett.rowan@icloud.com>
2024-04-24 08:57:00 -0600

    More string formatting.

--- lesson-1/src/main.rs
+++ lesson-1/src/main.rs
@@ -2,5 +2,8 @@ fn main() {
     let message = String::from("Hello, world!");
     let message_ref = &message;
 
-    println!("{}", *message_ref)
+    println!("{}", message_ref);
+    println!("{}", message_ref.to_uppercase());
+    println!("{}", message_ref.to_lowercase());
+    println!("{}", message_ref.replace("Hello", "Goodbye"));
 }

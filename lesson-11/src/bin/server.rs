83597006ff133cbc0e5e1adc7cc152bdd7da9b5f
b-rowan <brett.rowan@icloud.com>
2024-05-30 16:11:19 -0600

    Add lesson 11, with some comments and tracing.

---
+++ lesson-11/src/bin/server.rs
@@ -0,0 +1,136 @@
+use std::collections::HashMap;
+use std::error::Error;
+use std::io;
+use std::sync::Arc;
+use std::time::Duration;
+
+use clap::Parser;
+use parking_lot::FairMutex;
+use tokio::io::{AsyncReadExt, Interest};
+use tokio::net::{TcpListener, TcpStream};
+use tokio::sync::mpsc::{self, UnboundedSender};
+
+use lesson_11::Message;
+
+#[derive(Parser, Debug)]
+struct Args {
+    #[arg(short, long, default_value_t = String::from("0.0.0.0"))]
+    address: String,
+    #[arg(short, long, default_value_t = 11111)]
+    port: u16,
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let args = Args::parse();
+
+    let server = TcpListener::bind(format!("{}:{}", args.address, args.port)).await?;
+    let clients = Arc::new(FairMutex::new(HashMap::new()));
+    loop {
+        let (socket, addr) = server.accept().await?;
+        println!("Accepted client: {addr}");
+        tokio::spawn(handle_client(socket, clients.clone()));
+    }
+}
+
+async fn handle_client(
+    mut stream: TcpStream,
+    clients: Arc<FairMutex<HashMap<String, UnboundedSender<Message>>>>,
+) {
+    let (tx, mut rx) = mpsc::unbounded_channel::<Message>();
+    {
+        clients.lock().insert(stream.peer_addr().unwrap().to_string(), tx);
+    }
+    loop {
+        let ready = stream
+            .ready(Interest::READABLE | Interest::WRITABLE)
+            .await
+            .expect("Failed to initialized read/write checks...");
+
+        if ready.is_readable() {
+            let mut msg_length_raw = [0u8; 4];
+            let read_result = stream.try_read(&mut msg_length_raw);
+            if read_result.is_err() {
+                let result = read_result.err().unwrap();
+                if result.kind() == io::ErrorKind::WouldBlock {
+                    continue;
+                }
+                eprintln!("Error with {}: {result}", stream.peer_addr().unwrap());
+                continue;
+            }
+
+            let msg_length = u32::from_le_bytes(msg_length_raw);
+            let mut msg_raw = vec![
+                0u8;
+                usize::try_from(msg_length)
+                    .expect("Failed to parse message length from client...")
+            ];
+            stream
+                .read_exact(&mut msg_raw)
+                .await
+                .expect("Failed to read message from client...");
+
+            let message_result = serde_cbor::from_slice(&msg_raw);
+
+            let message: Message = match message_result {
+                Ok(msg) => msg,
+                Err(_) => {
+                    eprintln!(
+                        "Client {} closed the connection",
+                        stream.peer_addr().unwrap()
+                    );
+                    clients.lock().remove(&stream.peer_addr().unwrap().to_string());
+                    return;
+                }
+            };
+
+            match &message {
+                Message::File { name, .. } => {
+                    println!(
+                        "Receiving file from {}: {name}",
+                        stream.peer_addr().unwrap()
+                    );
+                }
+                Message::Photo { .. } => {
+                    println!("Receiving photo from {}", stream.peer_addr().unwrap());
+                }
+                Message::Text(text) => {
+                    println!("Got client message: {text}");
+                }
+                _ => {}
+            }
+
+            {
+                let client_handle = clients.lock();
+                for c in client_handle.values() {
+                    c.send(message.clone())
+                        .expect("Failed to broadcast message from client...");
+                }
+            }
+        }
+
+        let send_data = rx.try_recv();
+        if let Ok(data) = send_data {
+            loop {
+                let msg_serialized =
+                    serde_cbor::to_vec(&data).expect("Failed to serialize message for client...");
+                let msg_length = msg_serialized.len() as u32;
+
+                let write_res = stream.try_write(&msg_length.to_le_bytes());
+                if write_res.is_err() {
+                    let result = write_res.err().unwrap();
+                    if result.kind() == io::ErrorKind::WouldBlock {
+                        continue;
+                    }
+                    eprintln!("Error with {}: {result}", stream.peer_addr().unwrap());
+                    break;
+                }
+                stream
+                    .try_write(&msg_serialized)
+                    .expect("Failed to send message to client...");
+                break;
+            }
+        }
+        tokio::time::sleep(Duration::from_millis(1)).await;
+    }
+}
---
+++ lesson-11/src/lib.rs
@@ -0,0 +1,57 @@
+use image::io::Reader as ImageReader;
+use image::{DynamicImage, ImageFormat};
+use std::fs;
+use std::io::Cursor;
+use std::path::Path;
+
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub enum Message {
+    File { name: String, data: String },
+    Photo { data: Vec<u8> },
+    Text(String),
+    Stop,
+}
+
+impl From<String> for Message {
+    fn from(value: String) -> Self {
+        if value.starts_with(".") {
+            // handle command
+            let split_data: Vec<_> = value.splitn(2, " ").collect();
+            return match split_data[0] {
+                ".stop" => Message::Stop,
+                ".file" => {
+                    let filename = split_data[1];
+                    let file_path = Path::new(filename);
+                    if file_path.exists() {
+                        return Message::File {
+                            name: file_path.file_name().unwrap().to_string_lossy().to_string(),
+                            data: fs::read_to_string(&filename)
+                                .expect("Could not open the specified file."),
+                        };
+                    }
+                    Message::Text(value)
+                }
+                ".image" => {
+                    let filename = split_data[1];
+                    let file_path = Path::new(filename);
+
+                    let img: DynamicImage = ImageReader::open(file_path)
+                        .expect("Could not open the specified image.")
+                        .decode()
+                        .expect("Could not load the specified image.");
+                    let mut buf = Vec::new();
+                    img.write_to(&mut Cursor::new(&mut buf), ImageFormat::Png)
+                        .unwrap();
+                    if file_path.exists() {
+                        return Message::Photo { data: buf };
+                    };
+                    Message::Text(value)
+                }
+                _ => Message::Text(value),
+            };
+        }
+        Message::Text(value)
+    }
+}
